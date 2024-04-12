4bae574bb4c5c399e887219bf39e7bf53ab45b77
owaisyosuf <157351741+owaisyosuf@users.noreply.github.com>
2024-04-12 15:32:12 +0500

    todos list

---
+++ main.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = []; // todos empty list
+let condition = true;
+while (condition) {
+    // this is the item to add in todos
+    let answer = await inquirer.prompt([
+        {
+            name: "question1",
+            message: "what do you want to add in your todos list",
+            type: "input"
+        },
+        {
+            name: "question2",
+            message: "do you want to add more item in your todos list",
+            type: "confirm",
+            default: "true"
+        }
+    ]);
+    todos.push(answer.question1);
+    console.log(todos);
+    condition = answer.question2;
+}
